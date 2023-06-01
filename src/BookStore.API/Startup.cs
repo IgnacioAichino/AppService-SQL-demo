f83797b8f747ff369aa1609f7543ae3e2fb38838
Ignacio Aichino <ignacio.aichino@cedi.com.ar>
2023-06-01 13:25:06 -0300

    test

--- src/BookStore.API/Startup.cs
+++ src/BookStore.API/Startup.cs
@@ -36,7 +36,7 @@ namespace BookStore.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo()
                 {
-                    Title = "BookStore API ",
+                    Title = "BookStore API version ",
                     Version = "v2"
                 });
             });
